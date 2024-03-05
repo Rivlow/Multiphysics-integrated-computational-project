b94ea547316ef92dfd1e6c5e33dd6ba5788f289b
Killian <killian.kollasch@student.uliege.be>
2024-03-05 14:19:39 +0100

    sorted_list . cpp

--- sorted_list.cpp
+++ sorted_list.cpp
@@ -7,111 +7,123 @@
 #include <list>
 #include <unordered_map>
 #include <iostream>
+#include <chrono>
+#include <algorithm>
+
 using namespace std;
 
 
-// Définition de la structure pour représenter un triplet de valeurs doubles
-/*struct Triplet {
-    double x;
-    double y;
-    double z;
-
-    // Constructeur
-    Triplet(double x_val, double y_val, double z_val) : x(x_val), y(y_val), z(z_val) {}
-};*/
-
-
-/* Attention !! s'assurer que rand ne renvoie jamais les mêmes indices !! sinon conflit TO DO ! A FAIRE PLUS TARD !!! 
-void setRandomParticles(const unsigned &seed, const int &nb_particles, const double &Lx, const double &Ly, const double &Lz, vector<double> &particle_x, vector<double> &particle_y, vector<double> &particle_z){
-
-    // Uniform distribution 
-    mt19937 gen(seed);
-    uniform_real_distribution<double> dis_x(0.0, Lx); 
-    uniform_real_distribution<double> dis_y(0.0, Ly); 
-    uniform_real_distribution<double> dis_z(0.0, Lz); 
-
-    for (int i = 0; i < nb_particles; ++i) {
-        
-        double x = dis_x(gen); 
-        double y = dis_y(gen); 
-        double z = dis_z(gen); 
-
-        particle_x.push_back(x); 
-        particle_y.push_back(y); 
-        particle_z.push_back(z); 
-    }
-}*/
-
-
-void linkedList( vector<double> &particle_x, vector<double> &particle_y, vector<double> &particle_z, vector<unsigned> &particle_i, vector<unsigned> &particle_j, vector<unsigned> &particle_k, 
-                 vector<vector<int>> neighbours_matrix, const double &Lx, const double &Ly, const double &Lz, const int &Nx, const int &Ny, const int &Nz, const int &h){
+void findNeighbours(vector<double> &part_pos, vector<vector<unsigned>> &cell_pos,
+                 vector<vector<unsigned>> &neighbours_matrix, double L[3], const unsigned &Nx, const unsigned &Ny, const unsigned &Nz, const double &h, const int &kappa){
 
     // Sort all particles in their corresponding cell
-    for (unsigned ite = 0; ite < particle_x.size(); ite ++){
-                particle_i.push_back(particle_x[ite]/(Lx/Nx));
-                particle_j.push_back(particle_y[ite]/(Ly/Ny));
-                particle_k.push_back(particle_z[ite]/(Lz/Nz));
+    for (unsigned pos = 0; pos < part_pos.size()/3; pos ++){
+        
+        unsigned idx_i = part_pos[3*pos + 0] / (L[0] / Nx);
+        unsigned idx_j = part_pos[3*pos + 1] / (L[1] / Ny);
+        unsigned idx_k = part_pos[3*pos + 2] / (L[2] / Nz);
+
+        idx_i = (idx_i == Nx) ? idx_i - 1 : idx_i;
+        idx_j = (idx_j == Ny) ? idx_j - 1 : idx_j;
+        idx_k = (idx_k == Nz) ? idx_k - 1 : idx_k;
+
+        cell_pos[idx_i + Nx*idx_j + Ny*Nx*idx_k].push_back(pos);
     }
 
+
+
     // Find neighbours for each particle
-    for (unsigned x = 0; x < particle_x.size(); x++){
-        
-        // Determine in which cell the particle is a changer peut-être
-        unsigned i_cell = particle_x[x]/(Lx/Nx);
-        unsigned j_cell = particle_y[x]/(Ly/Ny);
-        unsigned k_cell = particle_z[x]/(Lz/Nz);
+    for (unsigned pos = 0; pos < part_pos.size()/3; pos ++){
 
-        unsigned i_inf = i_cell - 1;
-        unsigned i_supp = i_cell + 1;
-        if(i_cell == 0 ){
-                i_inf = 0;
-        }
-        if(i_cell == particle_x.size() - 1 ){
-                i_supp = i_cell;
-        }
+        // Determine in which cell the particle is
+        unsigned i_cell = part_pos[3*pos + 0] / (L[0] / Nx);
+        unsigned j_cell = part_pos[3*pos + 1] / (L[1] / Ny);
+        unsigned k_cell = part_pos[3*pos + 2] / (L[2] / Nz);
 
-        unsigned j_inf = j_cell - 1;
-        unsigned j_supp = j_cell + 1;
-        if(j_cell == 0 ){
-                j_inf = 0;
-        }
-        if(j_cell == particle_x.size() - 1 ){
-                j_supp = j_cell;
-        }
+        i_cell = (i_cell >= Nx) ? Nx-1 : i_cell;
+        j_cell = (j_cell >= Ny) ? Ny-1 : j_cell;
+        k_cell = (k_cell >= Nx) ? Nz-1 : k_cell;
 
-        unsigned k_inf = k_cell - 1;
-        unsigned k_supp = k_cell + 1;
-        if(k_cell == 0 ){
-                k_inf = 0;
-        }
-        if(k_cell == particle_x.size() - 1 ){
-                k_supp = k_cell;
-        }
+        // Define neighbouring cell indices
+        unsigned i_inf = (i_cell == 0) ? 0 : i_cell - 1;
+        unsigned i_supp = (i_cell < Nx - 1) ? i_cell +1 : (i_cell == Nx - 1) ? i_cell : i_cell - 1;
 
-            // Iterate over all 26 adjacents cells to find neighbours 
+        unsigned j_inf = (j_cell == 0) ? 0 : j_cell - 1;
+        unsigned j_supp = (j_cell < Ny - 1) ? j_cell +1 : (j_cell == Ny - 1) ? j_cell : j_cell - 1;
+
+        unsigned k_inf = (k_cell == 0) ? 0 : k_cell - 1;
+        unsigned k_supp = (k_cell < Nz - 1) ? k_cell +1 : (k_cell == Nz - 1) ? k_cell : k_cell - 1;
+
+        // Iterate over (max) 26 adjacents cells to find neighbours 
         for (unsigned i = i_inf; i <= i_supp; i++){
             for (unsigned j = j_inf; j <= j_supp; j++){
                 for (unsigned k = k_inf; k <= k_supp; k++){
-                    for (unsigned l = x+1; l < particle_i.size(); l++){ // l est pour savoir quand on itère sur la particule si la cellule est voisine ou non 
-                        if (particle_i[l] == i){
-                            if (particle_j[l] == j){
-                                if (particle_k[l] == k){
-                                    double rx, ry, rz, r2;
-                                    rx = (particle_x[x] - particle_x[l] )*(particle_x[x] - particle_x[l] );
-                                    ry = (particle_y[x] - particle_y[l] )*(particle_y[x] - particle_y[l] );
-                                    rz = (particle_z[x] - particle_z[l] )*(particle_z[x] - particle_z[l] );
-                                    r2 = rx + ry + rz;
-                                    if(r2<= k*k*h*h){
-                                        neighbours_matrix[x].push_back(l); 
-                                        neighbours_matrix[l].push_back(x);
-                                    }            
-                                }      //optimisation voisin 
+
+                    vector<unsigned> &actual_cell = cell_pos[i + j*Nx + k*Nx*Ny];  
+
+                    for (size_t idx_neighbour_it = 0 ; idx_neighbour_it < actual_cell.size(); idx_neighbour_it++) {
+                        unsigned actual_cell_value = actual_cell[idx_neighbour_it]; 
+
+                        if(actual_cell_value != pos){
+                            
+                            double rx, ry, rz, r2;
+                            rx = (part_pos[3*pos] - part_pos[3*actual_cell_value])*(part_pos[3*pos] - part_pos[3*actual_cell_value]);
+                            ry = (part_pos[3*pos + 1] - part_pos[3*actual_cell_value+1])*(part_pos[3*pos + 1] - part_pos[3*actual_cell_value+1]);
+                            rz = (part_pos[3*pos + 2] - part_pos[3*actual_cell_value+2])*(part_pos[3*pos + 2] - part_pos[3*actual_cell_value+2]);
+                            r2 = rx + ry + rz;
+                            if(r2 <= kappa*kappa*h*h){
+                                neighbours_matrix[pos].push_back(actual_cell_value); 
                             }
-                        }        
-                    }
+                        }      
+                    }              
                 }
             }
-        }        
+        } 
     }
+
 } 
 
+void naiveAlgo(vector<double> &part_pos, vector<vector<unsigned>> &neighbours_matrix, const double &h, const int &kappa){
+
+    // Find neighbours for each particle
+    for (unsigned i = 0; i < part_pos.size()/3; i++){
+        for (unsigned j = i+1; j < part_pos.size()/3; j++){
+
+
+            double rx, ry, rz, r2;
+            rx = (part_pos[3*i] - part_pos[3*j] )*(part_pos[3*i] - part_pos[3*j] );
+            ry = (part_pos[3*i+1] - part_pos[3*j+1] )*(part_pos[3*i+1] - part_pos[3*j+1] );
+            rz = (part_pos[3*i+2] - part_pos[3*j+2] )*(part_pos[3*i+2] - part_pos[3*j+2] );
+            r2 = rx + ry + rz;
+            if(r2<= kappa*kappa*h*h){
+                neighbours_matrix[i].push_back(j); 
+                neighbours_matrix[j].push_back(i);
+            }
+        }
+    }
+}
+
+void printNeighbours(vector<vector<unsigned>> &neighbours_matrix_1, vector<vector<unsigned>> &neighbours_matrix_2){
+
+    for (unsigned i = 0; i < neighbours_matrix_1.size(); i++) {
+        std::cout << "Particle " << i << " : ";
+
+        std::cout << "{";
+        for (unsigned j = 0; j < neighbours_matrix_1[i].size(); j++) {
+            std::cout << neighbours_matrix_1[i][j];
+            if (j != neighbours_matrix_1[i].size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "} (Linked-list) VS {";
+        
+        for (unsigned j = 0; j < neighbours_matrix_2[i].size(); j++) {
+            std::cout << neighbours_matrix_2[i][j];
+            if (j != neighbours_matrix_2[i].size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "} (naive)\n \n";
+        
+    }
+}
