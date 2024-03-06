7a0317436b23ee5f039337772dced7ccb6bac825
Killian <killian.kollasch@student.uliege.be>
2024-03-06 14:51:55 +0100

    Change grandient

--- gradient.cpp
+++ gradient.cpp
@@ -4,6 +4,8 @@
 #include "Kernel_functions.h"
 using namespace std;
 
+
+
 void gradW(const vector<double> &part_pos, const vector<vector<unsigned>> &neighbours_matrix, vector<vector<double>> &gradW_matrix, 
            double L[3], const double &h, const int &Nx, const int &Ny, const int &Nz){
 
@@ -32,28 +34,47 @@ void gradW(const vector<double> &part_pos, const vector<vector<unsigned>> &neigh
         gradW_matrix.push_back(gradW_vect);
         gradW_vect.clear();
     }
-
-    /*
-    for (size_t i = 0; i < gradW_matrix.size(); i++) {
-            std::cout << "Pour le " << i << "e element : (";
-            
-            for (size_t j = 0; j < gradW_matrix[i].size(); j++) {
-                std::cout << gradW_matrix[i][j];
-                
-                if (j < gradW_matrix[i].size() - 1) {
-                    std::cout << ", ";
-                }
-            }
-
-            std::cout << ")" << std::endl;
-        }
-    */
-    
 }
 
-void continuityEquation(const vector<double> &part_pos, const vector<vector<unsigned>> &neighbours_matrix, 
+double setArtificialViscosity(vector<vector<double>> &artificial_visc, const vector<double> &part_pos, const vector<vector<unsigned>> &neighbours_matrix, vector<double> &u_arr, 
+                            const double &c_ab, const double &rho_ab, const double &alpha, const double &beta, const double &h){
+
+
+    vector<double> rel_displ(3), rel_velo(3);
+
+    // Iterations over each particle
+    for (size_t pos = 0; pos < part_pos.size(); pos++){
+
+        const vector<unsigned> &neighbours_list = neighbours_matrix[pos];
+        for (size_t idx_neighbour = 0; idx_neighbour < neighbours_list.size(); idx_neighbour++){   
+
+            rel_displ[0] = (part_pos[3*pos+0] - part_pos[3*idx_neighbour+0]);
+            rel_displ[1] = (part_pos[3*pos+1] - part_pos[3*idx_neighbour+1]);
+            rel_displ[2] = (part_pos[3*pos+2] - part_pos[3*idx_neighbour+2]);
+
+            rel_velo[0] = (u_arr[3*pos+0] - u_arr[3*idx_neighbour+0]);
+            rel_velo[1] = (u_arr[3*pos+1] - u_arr[3*idx_neighbour+1]);
+            rel_velo[2] = (u_arr[3*pos+2] - u_arr[3*idx_neighbour+2]);
+
+            double res = 0, xa_xb = 0;
+            // Dot product
+            for (size_t idx = 0; idx < 3; idx++){
+                res += rel_velo[idx]*rel_displ[idx];
+                xa_xb += rel_displ[idx]*rel_displ[idx];
+            }
+
+            double nu_2 = 0.01*h*h;
+            double mu_ab = (h*res)/(xa_xb + nu_2);
+            artificial_visc[pos].push_back((res < 0) ? (-alpha*c_ab*mu_ab + beta*mu_ab*mu_ab)/rho_ab : 0);
+
+        }
+    }
+}
+
+void continuityEquation(const vector<double> &part_pos, const vector<double> &u_arr, const vector<vector<unsigned>> &neighbours_matrix, 
                         const vector<vector<double>> &gradW_matrix, vector<double> &drhodt_arr, vector<double> &rho_arr, const double &mass, const double &h){
 
+    // Iterations over each particle                    
     for (size_t pos = 0; pos < part_pos.size()/3; pos++){
 
         const vector<unsigned> &neighbours_list = neighbours_matrix[pos];
@@ -68,13 +89,10 @@ void continuityEquation(const vector<double> &part_pos, const vector<vector<unsi
             font bien reference au meme au meme voisin ???? Normalement oui mais pas sur a 100% "*/
 
             // Dot product of u_ab with grad_a(W_ab)
-
-
-            /* ATTENTION !!!! Ici j'ai mis "(x,y,z)" plutot que "(u_x,u_y,u_z)"" uniquement pour voir ce que ca affichait -> a changer plus tard*/
             double dot_product = 0;
             for (size_t x = 0; x < 3; x++){
 
-                dot_product += (part_pos[3*pos+x] - part_pos[3*neighbours_list[idx_neighbour]+x])*(gradW_list[idx_neighbour+x]);
+                dot_product += (u_arr[3*pos+x] - u_arr[3*neighbours_list[idx_neighbour]+x])*(gradW_list[idx_neighbour+x]);
             }
 
             double rx, ry, rz, r_ab;
@@ -84,8 +102,7 @@ void continuityEquation(const vector<double> &part_pos, const vector<vector<unsi
             r_ab = sqrt(rx + ry + rz);
 
             rho += mass*f_cubic_spline(r_ab, h);
-            drhodt += mass*dot_product; // mass of particles is constant ?? Or rather use mass_matrix (which would be set initially) in case of ?
-
+            drhodt += mass*dot_product;
         }
 
         rho_arr[pos] = rho;
@@ -93,21 +110,56 @@ void continuityEquation(const vector<double> &part_pos, const vector<vector<unsi
     }
 }
 
-void momentumEquation(const double &mass, const vector<vector<double>> &gradW_matrix, const vector<double> &rho_arr, const double &rho_0, 
-                      vector<double> &p_arr, const double &R, const double &T, const double &M, const string &state_equation_chosen){
+void momentumEquation(const vector<vector<unsigned>> &neighbours_matrix, const vector<double> &mass_arr, const vector<vector<double>> &gradW_matrix, 
+                      vector<double> &dudt_arr, vector<vector<double>> &artificial_visc, const vector<double> &rho_arr, const double &rho_0, const double &c_0,
+                      vector<double> &p_arr, const double &R, const double &T, const double &M, const double &gamma, const string &state_equation_chosen){
 
-    for (size_t a = 0; a < rho_arr.size(); a++)
-    double p = stateEquation(rho_arr[a], rho_0, R, T, M, state_equation_chosen);
+    // Iterations over each particle
+    for (size_t pos = 0; pos < rho_arr.size(); pos++){
+
+        const vector<unsigned> &neighbours_list = neighbours_matrix[pos];
+        const vector<double> &gradW_list = gradW_matrix[pos];
+
+        double p_a, c_a;
+        stateEquation(p_a, c_a, rho_arr[pos], rho_0, c_0, R, T, M, gamma, state_equation_chosen);
+
+        vector<double> dudt(3, 0.0);
+
+        // Summation over b = 1 -> nb_neighbours
+        for (size_t idx_neighbour = 0; idx_neighbour < neighbours_list.size(); idx_neighbour++){   
+
+            double p_b, c_b;
+            stateEquation(p_b, c_b, rho_arr[neighbours_list[idx_neighbour]], rho_0, c_0, R, T, M, gamma, state_equation_chosen);
+
+            double rho_a = rho_arr[pos], rho_b = rho_arr[neighbours_list[idx_neighbour]];
+            double rho_ab = 0.5*(rho_a + rho_b);
+            double pi_ab = artificial_visc[pos][neighbours_list[idx_neighbour]];
+            double m_b = mass_arr[neighbours_list[idx_neighbour]];
+
+            for (size_t it = 0; it < 3; it++) {dudt[it] += m_b*(p_b/(rho_b*rho_b) + p_a/(rho_a*rho_a) + pi_ab)*gradW_list[idx_neighbour+it];}
+        }
+        
+        for (size_t it = 0; it < 3; it++) {
+
+            dudt[it] *= -1; 
+            dudt_arr[3*pos+it] = dudt[it];
+        }
+
+    }
 }
 
 
-double stateEquation(const double &rho, const double &rho_0, const double &R, const double &T,
+void stateEquation(double &p, double &c, const double &rho, const double &rho_0, const double &c_0, const double &R, const double &T,
                      const double &M, const double &gamma, const string state_equation_chosen){
 
     if (state_equation_chosen == "Ideal gaz law"){
-        double p = (rho/rho_0 - 1)*(rho*R*T)/M;
+        p = (rho/rho_0 - 1)*(rho*R*T)/M;
+        c = c_0;
     }
     if (state_equation_chosen == "Quasi incompresible fluid"){
-        double p = (rho*R*T)/M;
+        double B = c_0*c_0*rho_0/gamma;
+        p = B*(pow(rho/rho_0, gamma) - 1);
+        c = c_0*pow(rho/rho_0, 0.5*(gamma-1));
     }
+
 }
