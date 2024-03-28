9cd72c4135b311cb09495126c1675e86236d3662
Luca <luca.santoro@trinotec.com>
2024-03-28 02:44:38 +0100

    kj

--- src/gradient.cpp
+++ src/gradient.cpp
@@ -284,7 +284,7 @@ void momentumEquation(vector<vector<int>> &neighbours_matrix,
             // Summation over b = 1 -> nb_neighbours
             for (size_t idx_neighbour = 0; idx_neighbour < neighbours_array.size(); idx_neighbour++)
             {
-                double pi_ab = 0; //artificial_visc_array[idx_neighbour];
+                double pi_ab = artificial_visc_array[idx_neighbour];
                 double rho_b = rho_array[neighbours_array[idx_neighbour]];
                 double m_b = mass_array[neighbours_array[idx_neighbour]];
                 double p_b = p_array[neighbours_array[idx_neighbour]];
