258cd9328fc4041d9eddd7b67d03dd00bfcaf94f
Killian <killian.kollasch@student.uliege.be>
2024-05-22 15:31:21 +0200

    changes

--- python/write_json.py
+++ python/write_json.py
@@ -2,46 +2,56 @@ import json
 import os
 import sys
 
-# Définir les données complètes du JSON
-
-s = 0.001
-L = 0.12
+s = 0.025
+L = 1.2
+Lz = 2
+dimension = 2
 
 data = {
     
     "simulation": {
         "theta": 0.5,
         "s": s,
-        "nstepT": 100000,
-        "dt": 0.00001,
+        "nstepT": 20000,
+        "dt": 0.0001,
         "nsave": 100,
         "kappa": 2,
-        "alpha": 0.5,
+        "alpha": 5,
         "beta": 0,
         "alpha_st": 10,
         "beta_adh": 1.2,
-        "dimension": 2
+        "dimension": dimension
     },
     
     "domain": {
         "matrix_long": [
-            [L/2, s/2, L/2], # fluid
+            [L-2*s, s/2, Lz-2*s], # fluid
             [L+s, s/2, s/2],  
             [L, s/2, s/2],
+            [s/2, s/2, (Lz)],
+            [s/2, s/2, (Lz-s)],
+            [s/2, s/2, Lz],
+            [s/2, s/2, Lz-s],
+
         ],
         "matrix_orig": [
-            [L/4+s/2, 0, L/4], # fluid
-            [0.0, 0, 0], 
-            [s/2, 0, s/2],  
+            [3*s/2, 0, 3*s/2], # fluid
+            [0, 0, 0], 
+            [s/2, 0, s/2], 
+            [0, 0, s],
+            [s*0.5, 0, 1.5*s],
+            [L+s, 0, s],
+            [L+s*0.5, 0, 1.5*s],
+
         ],
-        "vector_type": [1, 0, 0],
-        "L_d": [(L+s)*1.1, s, (L+s)*1.1],
+        "vector_type": [1, 0, 0, 0, 0, 0, 0],
+        "L_d": [2, 2, 2],
         "o_d": [0.0, 0.0, 0.0]
     },
     "post_process": {
         "do": False,
-        "xyz_init": [s, 0, L/5],
-        "xyz_end": [L+s, 0, L/5]
+        "xyz_init": [L/2, 0, s],
+        "xyz_end": [L/2, 0, L]
     },
     "thermo": {
         "rho_0": 1000,
@@ -49,7 +59,7 @@ data = {
         "rho_fixed": 1000,
         "T": 298.15,
         "u_init": [0.0, 0.0, 0.0],
-        "c_0": 340,
+        "c_0": 30,
         "gamma": 7,
         "M": 18e-3,
         "R": 8.314,
@@ -57,27 +67,24 @@ data = {
     },
     "forces": {
         "gravity": True,
-        "surface_tension": True,
-        "adhesion": True
+        "surface_tension": False,
+        "adhesion": False
     },
     "condition": {
         "print_debug": False,
         "schemeIntegration": {"Euler": True, "RK22": False},
         "stateEquation": {"Ideal gaz law": False, "Quasi incompresible fluid": True},
-        "initialCondition": {"Hydrostatic": False, "Constant": True}
+        "initialCondition": {"Hydrostatic": True, "Constant": False}
     }
 }
 
+
+    
 current_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
-print(current_directory)
+json_src = f"dam_break/{dimension}D_dam_break.json"
 
-print(f"{current_directory}/..")
-
-
-# Écrire les données dans un fichier JSON
-json_src = "surface_tension/2D_surface_tension.json"
 with open(f'{current_directory}/../tests/{json_src}', 'w') as json_file:
     json.dump(data, json_file, indent=4)
 
-print(f"Data written in '{json_src}'")
 
+print(f"Data written in '{json_src}'")
